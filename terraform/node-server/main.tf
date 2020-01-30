060d5c0c7eae53d3219c7b04c55f29db57f71ebd
Kyle Stewart <kgstew@gmail.com>
2020-01-29 23:05:00 -0800

    Update amazon install server

--- terraform/node-server/main.tf
+++ terraform/node-server/main.tf
@@ -17,7 +17,7 @@ nvm i 12.13.0
 curl -o- -L https://yarnpkg.com/install.sh | bash
 sudo yum install -y ruby wget
 cd /home/ec2-user
-wget https://aws-codedeploy-ap-southeast-2.s3.ap-southeast-2.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo ln -s /home/ec2-user/.nvm/versions/node/v12.13.0/bin/node /usr/bin
