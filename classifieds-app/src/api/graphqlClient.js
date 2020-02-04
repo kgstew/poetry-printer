cb475fb012b4d67ad8fc4a5d03d6060b44ecdf85
Kyle Stewart <kgstew@gmail.com>
2020-02-03 23:36:33 -0800

    Add error link to apolloClient

--- classifieds-app/src/api/graphqlClient.js
+++ classifieds-app/src/api/graphqlClient.js
@@ -1,15 +1,30 @@
-import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 export const cache = new InMemoryCache();
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) console.log('GraphQL Errors', graphQLErrors);
+    graphQLErrors.map(({ message, locations, path }) =>
+        console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+    );
+
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const httpLink = new HttpLink({
+    credentials: 'include',
+    uri: process.env.SERVICES_URI + '/graphql'
+});
+
+const link = ApolloLink.from([errorLink, httpLink]);
+
 const client = new ApolloClient({
-  cache,
-  link: new HttpLink({
-    credentials: "include",
-    uri: process.env.SERVICES_URI + "/graphql"
-  })
+    cache,
+    link
 });
 
 export default client;
